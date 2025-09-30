cfe76f7935c0154de5e60303650c1a5502b217cd
infofuturebound-afk <info.futurebound@gmail.com>
2025-09-30 21:49:01 +0530

    1

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+module pos.system {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+
+    opens com.devstack.pos to javafx.fxml;
+    exports com.devstack.pos;
+}
